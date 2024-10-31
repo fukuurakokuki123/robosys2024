f6b3f4a5bb20f417b896577c28a17571b4626302
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2024-10-31 12:51:31 +0900

    change code

--- test.bash
+++ test.bash
@@ -8,15 +8,7 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
 [ "${out}" = 15 ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
 
 [ "${res}" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
 exit $res
-+ exit 0
