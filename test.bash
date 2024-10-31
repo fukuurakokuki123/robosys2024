e87bbabe0e925f0f1837472c78e0debf088a5d9f
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2024-10-31 13:09:29 +0900

    change code

--- test.bash
+++ test.bash
@@ -1,8 +1,8 @@
 #!/bin/bash -xv
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+ echo ${1}行目が違うよ
+ res=1
 }
 
 res=0
@@ -10,5 +10,13 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]   || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]   || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
