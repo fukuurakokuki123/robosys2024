b8759351a4191f1265a565ec5c81e157475716b9
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2024-11-26 20:07:28 +0900

    tanomu

--- test.bash
+++ test.bash
@@ -10,19 +10,19 @@ ng () {
 res=0
 
 # 1つ目のテスト: 12 15が与えられた場合
-out=$(echo "12 15" | python3 ./kadai1.py)
+out=$(python3 ./kadai1.py 12 15)
 if [ "${out}" != "3" ]; then
     ng "$LINENO"
 fi
 
 # 2つ目のテスト: 非数値「あ」が与えられた場合
-out=$(echo "あ" | python3 ./kadai1.py)
+out=$(python3 ./kadai1.py あ)
 if [ "$?" -ne 1 ] || [ "${out}" != "少なくとも2つの数値を入力してください。" ]; then
     ng "$LINENO"
 fi
 
 # 3つ目のテスト: 引数なし
-out=$(echo "" | python3 ./kadai1.py)
+out=$(python3 ./kadai1.py)
 if [ "$?" -ne 1 ] || [ "${out}" != "少なくとも2つの数値を入力してください。" ]; then
     ng "$LINENO"
 fi
