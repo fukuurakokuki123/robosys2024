8081981f8dbc390a15729078dcb4acfcd57e99dc
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2024-11-26 19:57:17 +0900

    tanomu

--- kadai1.py
+++ kadai1.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 # SPDX-FileCopyrightText: 2024 Kouki Fukuura
 # SPDX-License-Identifier: BSD-3-Clause
+
 import sys
 
-# 標準入力からデータを取得
-try:
-    inputs = sys.stdin.read().strip().split()
-    if len(inputs) < 2:
-        print("少なくとも2つの数値を入力してください。")
-        sys.exit(1)
-    a, b = int(inputs[0]), int(inputs[1])
-except ValueError:
-    print("入力は数値でなければなりません。")
-    sys.exit(1)
-
-# GCD（最大公約数）を計算する関数
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+
+# 標準入力から2つの数値を読み取る
+try:
+    a, b = map(int, input().split())
+except ValueError:
+    print("少なくとも2つの数値を入力してください。", file=sys.stderr)
+    sys.exit(1)
 
 # GCDを計算して結果を表示
 print(gcd(a, b))
