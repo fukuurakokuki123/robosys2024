d606e93afa835bd125f74be535752fbb74d7b828
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2024-11-26 19:46:09 +0900

    tanomu

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-/bin/bash -xv
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Kouki Fukuura
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -8,44 +8,30 @@ ng () {
 }
 
 res=0
-+ res=0
 
-# 1つ目のテスト
+# 1つ目のテスト: 12 15が与えられた場合
 out=$(python3 ./kadai1.py 12 15)
-++ python3 ./kadai1.py 12 15
-+ out=3
 if [ "${out}" != "3" ]; then
     ng "$LINENO"
 fi
-+ '[' 3 '!=' 3 ']'
 
-# 2つ目のテスト: 非数値の場合
+# 2つ目のテスト: 非数値「あ」が与えられた場合
 out=$(python3 ./kadai1.py あ)
-++ python3 ./kadai1.py あ
-+ out=少なくとも2つの数値を入力してください。
 if [ "$?" -ne 1 ] || [ "${out}" != "少なくとも2つの数値を入力してください。" ]; then
     ng "$LINENO"
 fi
-+ '[' 1 -ne 1 ']'
-+ '[' 少なくとも2つの数値を入力してください。 '!=' 少なくとも2つの数値を入力してください。 ']'
 
 # 3つ目のテスト: 引数なし
 out=$(python3 ./kadai1.py)
-++ python3 ./kadai1.py
-+ out=少なくとも2つの数値を入力してください。
 if [ "$?" -ne 1 ] || [ "${out}" != "少なくとも2つの数値を入力してください。" ]; then
     ng "$LINENO"
 fi
-+ '[' 1 -ne 1 ']'
-+ '[' 少なくとも2つの数値を入力してください。 '!=' 少なくとも2つの数値を入力してください。 ']'
 
+# 結果の出力
 if [ "$res" = 0 ]; then
     echo "OK: All tests passed"
 else
     echo "NG: Some tests failed"
 fi
-+ '[' 0 = 0 ']'
-+ echo 'OK: All tests passed'
 
 exit $res
-+ exit 0
