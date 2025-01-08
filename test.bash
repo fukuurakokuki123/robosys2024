d3755376bdfc4eae1b34824a9d40ff54e5981fda
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2025-01-08 16:05:01 +0900

    gcd

--- test.bash
+++ test.bash
@@ -10,7 +10,7 @@ ng () {
 res=0
 
 # 1つ目のテスト: 12 15が与えられた場合
-out=$(echo "12 15" | python3 ./gcd.py)
+out=$(echo "12 15" | python3 ./gcd)
 expected="GCD: 3"
 echo "Output for '12 15': $out"
 if [ "$out" != "$expected" ]; then
@@ -18,7 +18,7 @@ if [ "$out" != "$expected" ]; then
 fi
 
 # 2つ目のテスト: 非数値「あ」が与えられた場合
-out=$(echo "あ あ" | python3 ./gcd.py)
+out=$(echo "あ あ" | python3 ./gcd)
 expected="エラー: 入力は数値でなければなりません。"
 echo "Output for 'あ あ': $out"
 if [ "$out" != "$expected" ]; then
@@ -26,7 +26,7 @@ if [ "$out" != "$expected" ]; then
 fi
 
 # 3つ目のテスト: 引数なし
-out=$(echo "" | python3 ./gcd.py)
+out=$(echo "" | python3 ./gcd)
 expected="エラー: 2つ以上の数値を入力してください"
 echo "Output for no arguments: $out"
 if [ "$out" != "$expected" ]; then
@@ -34,7 +34,7 @@ if [ "$out" != "$expected" ]; then
 fi
 
 # 4つ目のテスト: 10 20 30 40の場合
-out=$(echo "10 20 30 40" | python3 ./gcd.py)
+out=$(echo "10 20 30 40" | python3 ./gcd)
 expected="GCD: 10"
 echo "Output for '10 20 30 40': $out"
 if [ "$out" != "$expected" ]; then
