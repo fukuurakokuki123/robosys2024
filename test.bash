9bc718043ad28135beb0cda0e598a1a168c3295c
Kouki Fukuura <s23c1121bp@s.chibakoudai.jp>
2025-01-08 16:01:54 +0900

    gcd

--- test.bash
+++ test.bash
@@ -10,25 +10,33 @@ ng () {
 res=0
 
 # 1つ目のテスト: 12 15が与えられた場合
-out=$(echo "12 15" | python3 ./gcd)
-expected="2つの数値を入力してください: GCD: 3"
+out=$(echo "12 15" | python3 ./gcd.py)
+expected="GCD: 3"
 echo "Output for '12 15': $out"
 if [ "$out" != "$expected" ]; then
     ng "$LINENO"
 fi
 
 # 2つ目のテスト: 非数値「あ」が与えられた場合
-out=$(echo "あ あ" | python3 ./gcd)
-expected="2つの数値を入力してください: エラー: 入力は数値でなければなりません。"
+out=$(echo "あ あ" | python3 ./gcd.py)
+expected="エラー: 入力は数値でなければなりません。"
 echo "Output for 'あ あ': $out"
 if [ "$out" != "$expected" ]; then
     ng "$LINENO"
 fi
 
 # 3つ目のテスト: 引数なし
-out=$(echo "" | python3 ./gcd)
-expected="2つの数値を入力してください: エラー: 入力は数値でなければなりません。"
+out=$(echo "" | python3 ./gcd.py)
+expected="エラー: 2つ以上の数値を入力してください"
 echo "Output for no arguments: $out"
+if [ "$out" != "$expected" ]; then
+    ng "$LINENO"
+fi
+
+# 4つ目のテスト: 10 20 30 40の場合
+out=$(echo "10 20 30 40" | python3 ./gcd.py)
+expected="GCD: 10"
+echo "Output for '10 20 30 40': $out"
 if [ "$out" != "$expected" ]; then
     ng "$LINENO"
 fi
@@ -41,5 +49,3 @@ else
 fi
 
 exit $res
-
-
